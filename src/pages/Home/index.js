586fafe705babbc77348923db3eb320859e5c6c2
joao paulo <joaopaulopmedeiros@gmail.com>
2020-10-05 12:23:15 -0300

    getting started home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,13 +1,41 @@
-import React from "react"
+import React from "react";
+import { Link } from "react-router-dom";
+
+import logo from "../../assets/logo.png";
+import community from "../../assets/community.svg";
 
 import "./styles.css";
 
 const Home = () => {
-    return (
-        <div className="fluid-container">
-            <h1>Home</h1>
+  return (
+    <div className="fluid-container">
+      <header className="header">
+        <div className="brand">
+            <img src={logo} alt="logo" className="logo" />
         </div>
-    )
-}
+        <Link to="/login" className="login-btn">
+          Login
+        </Link>
+        <Link to="/register" className="register-btn">
+          Sign-up
+        </Link>
+      </header>
+      <div className="hero">
+        <div className="txt">
+          <h1>
+            Conecte-se. Ajude <br />e evolua.
+          </h1>
+          <h2>No DevHelper, você não está sozinho.</h2>
+          <Link to="/" className="entry-btn">
+            Embarcar
+          </Link>
+        </div>
+        <div className="img">
+          <img src={community} alt="Comunidade" />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
